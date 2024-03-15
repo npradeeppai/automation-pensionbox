cc4fe513fd0bfff906107575a7bac2f48db4fb35
pradeep <npradeeppai@gmail.com>
2024-03-15 12:49:20 +0530

    initial

---
+++ src/main/java/in/pensionbox/Main.java
@@ -0,0 +1,7 @@
+package in.pensionbox;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/TestNGCheck.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNGCheck {
+    @Test
+    public void Sample1(){
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+}
