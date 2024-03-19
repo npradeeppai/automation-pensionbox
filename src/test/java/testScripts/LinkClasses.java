affb3002e0fd1f8a91af63df09580d49495f4fff
pradeep <npradeeppai@gmail.com>
2024-03-19 15:50:15 +0530

    Added SignIn Pom and Suite changes

---
+++ src/test/java/testScripts/LinkClasses.java
@@ -0,0 +1,10 @@
+package testScripts;
+
+import generic.BaseClass;
+
+public class LinkClasses extends BaseClass{
+    public static void main(String[] args) {
+        Login lT=new Login();
+        lT.login();
+    }
+}
